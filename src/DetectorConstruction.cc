2e066b1b3abb8d81f8b2b709bc899949f5ba2f43
maoderos <marcos.deros@ufrgs.br>
2022-08-30 19:05:14 -0300

    changes in the command line added. Chip materials defined

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -1,4 +1,5 @@
 #include "DetectorConstruction.hh"
+#include "G4MTRunManager.hh"
 #include "G4NistManager.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
@@ -13,7 +14,7 @@
 DetectorConstruction::DetectorConstruction()
 : G4VUserDetectorConstruction(), logicWorld(0),physWorld(0), worldMaterial(0), solidMetal(0), logicMetal(0), physMetal(0),metalMaterial(0), metalThickness(10.*um/2),
 solidSensitive(0), logicSensitive(0), physSensitive(0), sensitiveMaterial(0), sensitiveThickness(25.*um/2), solidSubstrate(0), logicSubstrate(0),
-physSubstrate(0), substrateMaterial(0), substrateThickness(15.*um/2), Air(0), Silicon(0), Metal(0), targetGeometry("Chip"), chip_half_x(1.5*cm/2), chip_half_y(3.0*cm/2) 
+physSubstrate(0), substrateMaterial(0), substrateThickness(15.*um/2), Air(0), Silicon(0), Metal(0), targetGeometry("Chip"), chip_half_x(1.5*cm/2), chip_half_y(3.0*cm/2)
 {
     detectorMessenger = new DetectorMessenger(this);
     DefineMaterials();
@@ -25,30 +26,43 @@ DetectorConstruction::~DetectorConstruction(){
 }
 
 void DetectorConstruction::DefineMaterials(){
-     G4cout << "a" << G4endl;
+
     // Get materials from NIST database
     G4NistManager* nist = G4NistManager::Instance();
 
+    // Creating world material - Vaccumm
     worldMaterial = nist->FindOrBuildMaterial("G4_Galactic");
+
+    // Air
+    Air = nist->FindOrBuildMaterial("G4_AIR");
+
+    // Metal
+    G4Material* Al = nist->FindOrBuildMaterial("G4_Al");
     Silicon = nist->FindOrBuildMaterial("G4_Si");
-    
-    metalMaterial = Silicon;
-    sensitiveMaterial = Silicon;
+    G4Material* Oxygen = nist->FindOrBuildMaterial("G4_O");
+
+    metalMaterial = new G4Material("Metal", 2.28*g/cm3, 3); // (name, density, number of components)
+    metalMaterial->AddMaterial(Al, 17*perCent);
+    metalMaterial->AddMaterial(Silicon, 38.8*perCent);
+    metalMaterial->AddMaterial(Oxygen, 44.2*perCent);
+
+    sensitiveMaterial = Air;
     substrateMaterial = Silicon;
-    
+
+    // One can add the Silicon Carbide material here using the same logic
+
 }
 
 G4VPhysicalVolume* DetectorConstruction::Construct(){
 
     //World Geometry
-    G4cout << "a" << G4endl;
     G4double halfsize_xy = 4*cm;
     G4double halfsize_z = 2*cm;
 
     G4Box* solidWorld = new G4Box("BoxWorld", halfsize_xy, halfsize_xy, halfsize_z);
     logicWorld = new G4LogicalVolume(solidWorld, worldMaterial,"logicWorld");
     physWorld = new G4PVPlacement(0,G4ThreeVector(), logicWorld, "World",0,false,0);
-  
+
     // Target geometry
     if (targetGeometry == "Chip") {
       G4cout << "Drawing chip" << G4endl;
@@ -57,7 +71,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
       solidMetal = new G4Box("boxMetal", chip_half_x, chip_half_y, metalThickness);
       logicMetal = new G4LogicalVolume(solidMetal, metalMaterial, "logicMetal");
       physMetal = new G4PVPlacement(0,G4ThreeVector(0,0,z_position), logicMetal, "Metal", logicWorld, false, 0, true);
-      // Sensitive volume 
+      // Sensitive volume
       z_position += metalThickness + sensitiveThickness;
       solidSensitive = new G4Box("boxSensitive", chip_half_x, chip_half_y, sensitiveThickness);
       logicSensitive = new G4LogicalVolume(solidSensitive, sensitiveMaterial, "logicSensitive");
@@ -70,16 +84,16 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
 
       // Metal and substrate visualization
       G4VisAttributes* metalVis = new G4VisAttributes();
-      metalVis->SetColor(1.0, 1.0, 0); //yellow 
+      metalVis->SetColor(1.0, 1.0, 0); //yellow
       logicMetal->SetVisAttributes(metalVis);
 
       G4VisAttributes* substrateVis = new G4VisAttributes();
-      substrateVis->SetColor(0.0, 1.0, 0); 
+      substrateVis->SetColor(0.0, 1.0, 0);
       logicSubstrate->SetVisAttributes(substrateVis);
 
 
-    } else if (targetGeometry == "Target") {
-      G4cout << "Drawing Target" << G4endl;
+    } else if (targetGeometry == "Material") {
+      G4cout << "Drawing Material" << G4endl;
 
       G4double z_position = sensitiveThickness;
       solidSensitive = new G4Box("boxSensitive", chip_half_x, chip_half_y, sensitiveThickness);
@@ -87,19 +101,19 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
       physSensitive = new G4PVPlacement(0,G4ThreeVector(0,0,z_position), logicSensitive, "Sensitive", logicWorld, false, 0, true);
 
     }
-    
+
     // World Visualization
     G4VisAttributes* worldVis = new G4VisAttributes();
     worldVis->SetColor(0.1, 0.3, 0.8, 0.5);
     logicWorld->SetVisAttributes(worldVis);
-   
-    //Sensitive visualization 
+
+    //Sensitive visualization
     G4VisAttributes* sensitiveVis = new G4VisAttributes();
-    sensitiveVis->SetColor(1.0, 0.0, 0); //red 
+    sensitiveVis->SetColor(1.0, 0.0, 0); //red
     logicSensitive->SetVisAttributes(sensitiveVis);
 
 
-    
+
     return physWorld;
 
 }
@@ -110,26 +124,32 @@ void DetectorConstruction::ConstructSDandField()
 }
 
 void DetectorConstruction::SetTargetGeometry(const G4String& value){
-  /*
-  targetGeometry = value;
-  if (logicTarget) {
-    if (targetGeometry == logicTarget->GetSolid()->GetName()){
+  if (logicMetal) {
+    if (targetGeometry == value){
       G4cout << "Geometry already selected in simulation." << G4endl;
     } else {
+      G4cout << "changing target geometry" << G4endl;
+      targetGeometry = value;
       G4MTRunManager::GetRunManager()->ReinitializeGeometry();
     }
   }
-  */
-  G4cout << "changing target geometry" << G4endl;
 }
 
 void DetectorConstruction::SetSensitiveThickness(G4double value){
-  //sensitiveThickness = value;
-  //G4MTRunManager::GetRunManager()->ReinitializeGeometry();
+  sensitiveThickness = value;
   G4cout << "changing sensitive volume thickness" << G4endl;
+  G4MTRunManager::GetRunManager()->ReinitializeGeometry();
 }
 
 G4bool DetectorConstruction::SetSensitiveMaterial(const G4String& value) {
-  G4cout << "changing sensitive volume material" << G4endl;
+  if (value == "Si") {
+    sensitiveMaterial = Silicon;
+  } // Add SiC here in an else-if
+  if (logicSensitive){
+    logicSensitive->SetMaterial(sensitiveMaterial);
+    G4MTRunManager::GetRunManager()->PhysicsHasBeenModified();
+    G4MTRunManager::GetRunManager()->GeometryHasBeenModified();
+    G4cout << "The material of the sensitive volume has been changed to " << sensitiveMaterial << G4endl;
+  }
   return true;
 }
